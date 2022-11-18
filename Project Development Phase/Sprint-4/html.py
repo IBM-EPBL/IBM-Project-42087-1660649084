1d4fb5b678a707e2df61a5a3bbf6d5b3c607b12b
ayyappan14 <113327133+ayyappan14@users.noreply.github.com>
2022-11-18 08:03:12 -0500

    Update html.py

--- Project Development Phase/Sprint-4/html.py
+++ Project Development Phase/Sprint-4/html.py
@@ -1,1 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>output</title>
+	<link rel="stylesheet" href= "https:maxcdn.boorstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+	<style>
+	     body
+	     {
+                background-image:url("https://www.apptunix.com/blog/wp-content/uploads/sites/3/2021/04/show-chatbots-for-banking.jpg");
+                background-size:cover;
+             }
+        </style>
+</head>
+  <script>
+  window.watsonAssistantChatOptions = {
+    integrationID: "c8d8e980-87ad-42fa-be7b-8bb6d80df4d7", // The ID of this integration.
+    region: "us-south", // The region your integration is hosted in.
+    serviceInstanceID: "14369731-970e-482b-babd-16c7bff81802", // The ID of your service instance.
+    onLoad: function(instance) { instance.render(); }
+  };
+  setTimeout(function(){
+    const t=document.createElement('script');
+    t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
+    document.head.appendChild(t);
+  });
+</script>
+
+
+</body>
+</html>         
 
