b1bd46c6d4cc46c7ea8842eb131b25c3e7e99137
ayyappan14 <113327133+ayyappan14@users.noreply.github.com>
2022-11-18 08:04:22 -0500

    Create python flask.py

---
+++ Project Development Phase/Sprint-4/python flask.py
@@ -0,0 +1,8 @@
+from flask import Flask,render_template
+  app=Flask(__name__)
+ @app.route("/bank")
+  def index():
+      return render_template('website.html')
+  if __name__ == "__main__":
+      app.run()
+
